24cd9130e9d6f83c654ae6ac8c9d794019d770eb
Sarang <sarang.rajurkar19@vit.edu>
2023-03-20 13:28:39 +0530

    refactoring

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,16 +2,6 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
-API.interceptors.request.use((req) => {
-  if (localStorage.getItem("user_info")) {
-    req.headers.Authorization = `Bearer ${JSON.parse(
-      localStorage.getItem("user_info").token
-    )}`;
-  }
-
-  return req;
-});
-
 export const signInGoogle = (accessToken) =>
   API.post("/users/signin", {
     googleAccessToken: accessToken,
